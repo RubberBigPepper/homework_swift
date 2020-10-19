59f02b357caaa7ed1df0f014752d2a728849bd46
Albert <rubberbigpepper@gmail.com>
2020-10-19 22:35:26 +0500

    RxSwift сильно отличается от Bond

---
+++ HomeWork2_3/TableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TableViewController.swift
+//  HomeWork2_3
+//
+//  Created by Albert on 19.10.2020.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func addPressed(_ sender: Any) {
+    }
+    @IBAction func removePressed(_ sender: Any) {
+    }
+    
+}
--- HomeWork2_3/ViewController.swift
+++ HomeWork2_3/ViewController.swift
@@ -9,12 +9,31 @@ import UIKit
 import RxSwift
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var sendBtn: UIButton!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var incrementLabel: UILabel!
+    @IBOutlet weak var labelRocket: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        loginTextField.rx.text.orEmpty
+            .bind	
     }
 
 
+    @IBAction func inscrementPressed(_ sender: Any) {
+    }
+    
+    @IBAction func btnFirstPressed(_ sender: Any) {
+    }
+    
+    @IBAction func btnSecondPressed(_ sender: Any) {
+    }
 }
 
