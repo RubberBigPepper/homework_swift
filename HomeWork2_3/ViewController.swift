fc9906aeaf0c9bb1f9d7cc8cbd61573974e3138a
Albert <rubberbigpepper@gmail.com>
2020-10-19 11:17:43 +0500

    Initial Commit

---
+++ HomeWork2_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2_3
+//
+//  Created by Albert on 19.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
